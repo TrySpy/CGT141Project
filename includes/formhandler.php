a8e96ba932348cb8e31a614d514452531cf60fad
TrySpy <bbone2000@hotmail.com>
2023-12-10 16:07:42 -0500

    tweak

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -6,11 +6,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
-    echo '<br>';
-    echo $name;
-    echo '<br>';
-    echo $email;
-    echo '<br>';
-    echo $message;
+    header("Location: ../index.html");
+}
+else {
+    header("Location: ../index.html")
 }
 ?>
