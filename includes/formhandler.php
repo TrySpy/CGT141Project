a5a0f2602894639a65b5c2c5d4cf0fd2e8b94af9
TrySpy <bbone2000@hotmail.com>
2023-12-10 16:51:54 -0500

    updated form, updated intrasite link

--- includes/formhandler.php
+++ includes/formhandler.php
@@ -2,8 +2,10 @@
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    $name = htmlspecialchars($_POST['name']);
+    $fname = htmlspecialchars($_POST['fname']);
+    $lname = htmlspecialchars($_POST['lname']);
     $email = htmlspecialchars($_POST['email']);
+    $sex = htmlspecialchars($_POST['sex']);
     $message = htmlspecialchars($_POST['message']);
 
     header("Location: ../index.html");
