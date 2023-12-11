6b31a029264846f4f9f34b6e0cc83279de2c4723
TrySpy <bbone2000@hotmail.com>
2023-12-10 19:32:28 -0500

    final

---
+++ proj/www/README.md
@@ -0,0 +1,3 @@
+# CGT 14100 JIT Project WIP
+# Brian Bone and Isaac Cheung
+# Fall Semester 2023 
---
+++ proj/www/includes/formhandler.php
@@ -0,0 +1,16 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $fname = htmlspecialchars($_POST['fname']);
+    $lname = htmlspecialchars($_POST['lname']);
+    $email = htmlspecialchars($_POST['email']);
+    $sex = htmlspecialchars($_POST['sex']);
+    $message = htmlspecialchars($_POST['message']);
+
+    header("Location: ../index.html");
+}
+else {
+    header("Location: ../index.html");
+}
+?>
